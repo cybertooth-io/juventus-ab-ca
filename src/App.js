d16cc27fbf4e5a367ef8a6be0cdd3a5d92f82bd9
Dan Nelson <dnelson@cybertooth.ca>
2018-09-14 13:36:17 -0600

    #time 1h 32m Add new bootstrap markup and bootstrap js.  Removed CRA boilerplate css and logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './styles/css/index.css';
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+        <div>
+          <section>
+            <nav className="navbar navbar-expand-md navbar-light bg-light">
+              <a className="navbar-brand" href="#">juventus-ab-ca</a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse"
+                      data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                      aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              </div>
+            </nav>
+          </section>
+          <section>
+            <div className="container">
+              <h1>Welcome</h1>
+            </div>
+          </section>
         </div>
     );
   }
